c3a0b219905412c12fbbe21971740e7f93eaa804
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-03-19 11:57:05 +0100

    feat(API): create GET /user endpoint for getting user data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -8,4 +9,5 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [UserController::class, 'showCurrentUser']);
 });
