0685a866f7ad9da6033963c380a307a3ccfd623e
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-03-21 12:34:12 +0100

    front: cleanup frontend & prepare empty login, profile and dashboard pages

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::get('/login', function() {
+    return Inertia::render('login');
+})->name('login');
+
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    return Inertia::render('dashboard');
+})->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+Route::get('profile', function () {
+    return Inertia::render('profile');
+})->name('profile');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
+
+// require __DIR__.'/settings.php';
+// require __DIR__.'/auth.php';
