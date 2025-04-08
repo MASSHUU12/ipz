04144554107952a481b7b786aa8284e50d195b2d
jhospo <janhospodarysko11@gmail.com>
2025-04-08 20:05:21 +0200

    Update custom.blade.php

--- resources/views/emails/custom.blade.php
+++ resources/views/emails/custom.blade.php
@@ -83,10 +83,6 @@
             margin-bottom: 30px;
         }
 
-        .logo img {
-            height: 50px;
-        }
-
         .highlight {
             background-color: #f8f9fa;
             padding: 20px;
@@ -99,13 +95,6 @@
 <body>
 <div class="container">
     <div class="email-card">
-        <div class="logo">
-            <!-- Tutaj możesz wstawić swoje logo -->
-            <svg width="50" height="50" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#3498db" stroke-width="2"/>
-                <path d="M8 14C8 14 9.5 16 12 16C14.5 16 16 14 16 14M9 9H9.01M15 9H15.01" stroke="#9b59b6" stroke-width="2" stroke-linecap="round"/>
-            </svg>
-        </div>
 
         <h1>Weryfikacja Adresu Email</h1>
 
