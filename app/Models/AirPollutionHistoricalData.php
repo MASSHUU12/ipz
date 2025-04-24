8d39d7eb0a9a57c816126aad09a96082c402523c
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-04-24 11:48:12 +0200

    feat(backend): create AirPollutionHistoricalData model

---
+++ app/Models/AirPollutionHistoricalData.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AirPollutionHistoricalData extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'air_pollution_historical_data';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'latitude',
+        'longitude',
+        'station_name',
+        'air_quality_index',
+        'pm10',
+        'pm25',
+        'no2',
+        'so2',
+        'o3',
+        'co',
+        'measurements',
+        'forecasts',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'pm10' => 'float',
+        'pm25' => 'float',
+        'no2' => 'float',
+        'so2' => 'float',
+        'o3' => 'float',
+        'co' => 'float',
+        'measurements' => 'array',
+        'forecasts' => 'array',
+    ];
+}
