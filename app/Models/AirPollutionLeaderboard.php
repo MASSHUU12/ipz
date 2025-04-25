caa60d1888f486520214b9b5fba8f004994159c6
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-04-25 19:36:33 +0200

    feat(backend): create AirPollutionLeaderboard model

---
+++ app/Models/AirPollutionLeaderboard.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AirPollutionLeaderboard extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'air_pollution_leaderboard';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'rank',
+        'station_name',
+        'city',
+        'air_quality_index',
+        'pm10',
+        'pm25',
+        'no2',
+        'so2',
+        'o3',
+        'co',
+        'timestamp',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+     protected $casts = [
+        'rank' => 'integer',
+        'station_name' => 'string',
+        'city' => 'string',
+        'air_quality_index' => 'string',
+        'pm10' => 'float',
+        'pm25' => 'float',
+        'no2' => 'float',
+        'so2' => 'float',
+        'o3' => 'float',
+        'co' => 'float',
+        'timestamp' => 'datetime',
+     ];
+}
