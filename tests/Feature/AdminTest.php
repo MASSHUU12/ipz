bcd2b113eec24dba0f77a9f2b59966fc36b6c81d
PrzemekT <przemek8319@gmail.com>
2025-05-03 19:40:03 +0200

    add admin test

---
+++ tests/Feature/AdminTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Database\Seeders\RolesAndPermissionsSeeder;
+
+class AdminTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Zasadź role i uprawnienia
+        $this->seed(RolesAndPermissionsSeeder::class);
+    }
+
+    /** @test */
+    public function testRenderAdminPage()
+    {
+        $response = $this->get('/idkfa');
+
+        $response->assertStatus(200)
+            ->assertInertia(
+                fn(Assert $page) =>
+                $page->component('admin')
+            );
+    }
+
+    /** @test */
+    public function testAccessAdmintestAsSuperAdmin()
+    {
+        // Utwórz użytkownika i nadaj mu rolę Super Admin
+        $user = User::factory()->create();
+        $user->assignRole('Super Admin');
+
+        $response = $this->actingAs($user, 'sanctum')
+            ->getJson('/api/admintest');
+
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => 'Sphinx of black quartz, judge my vow',
+            ]);
+    }
+
+    /** @test */
+    public function testForbiddenAccessAdmintestAsRegularUser()
+    {
+        // Utwórz użytkownika z rolą User
+        $user = User::factory()->create();
+        $user->assignRole('User');
+
+        $response = $this->actingAs($user, 'sanctum')
+            ->getJson('/api/admintest');
+
+        $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function testUnauthorizedAccessAdmintestWithoutAuthentication()
+    {
+        $response = $this->getJson('/api/admintest');
+
+        $response->assertStatus(401);
+    }
+}
