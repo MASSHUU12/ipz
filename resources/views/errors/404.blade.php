515f0c377bfe343cd0e79c684a13dc1bd30e5997
ErDeIgRek1 <162823644+ErDeIgRek1@users.noreply.github.com>
2025-05-03 12:02:21 +0200

    404 poprawka na blade'a

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,8 +5,7 @@
   <meta charset="UTF-8">
   <title>404 - Not Found</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  {{-- Tailwind lub własne style, jeśli używasz --}}
+  
   <style>
     body {
       margin: 0;
