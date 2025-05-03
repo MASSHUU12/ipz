2290687ea4e261806bdd66f917ae2dc8fcd9eee7
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-05-03 11:24:38 +0200

    feat(fe): create wrapper for /air-quality endpoint

---
+++ resources/js/api/airQualityApi.ts
@@ -0,0 +1,71 @@
+import { instance } from "./api";
+
+export interface Coords {
+  latitude: number;
+  longitude: number;
+}
+
+export interface Station {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  address: string;
+  city: string;
+  district: string;
+  province: string;
+}
+
+export interface Pollutant {
+  value: number | null;
+}
+
+export interface AirQuality {
+  index: string;
+  pollutants: {
+    pm10: Pollutant;
+    pm25: Pollutant;
+    no2: Pollutant;
+    so2: Pollutant;
+    o3: Pollutant;
+    co: Pollutant;
+  };
+}
+
+export interface Measurement {
+  date: string;
+  value: number;
+}
+
+export interface Measurements {
+  [key: string]: Measurement[];
+}
+
+export interface AirQualityResponse {
+  success: boolean;
+  timestamp: string;
+  request: Coords;
+  station: Station;
+  airQuality: AirQuality;
+  measurements: Measurements[];
+  forecasts: unknown[];
+}
+
+export const getAirQuality = async (
+  address: string,
+): Promise<AirQualityResponse | null> => {
+  // TODO: Recognise coords, for now everything is address
+  try {
+    const response = await instance.get<AirQualityResponse>(
+      `/air-quality?addr=${address}`,
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error);
+    } else {
+      console.error("An unknown error occurred.");
+    }
+    return null;
+  }
+};
--- resources/js/data/useAirQuality.ts
+++ resources/js/data/useAirQuality.ts
@@ -1,80 +1,23 @@
 import { useEffect, useState } from "react";
-import { cityCoordinates } from "./cities";
-import { instance } from "@/api/api";
-
-export interface AirQualityData {
-  airQuality: {
-    index: string;
-  };
-  measurements: {
-    parameter: string;
-    value: number;
-    unit: string;
-    measurementTime: string;
-  }[];
-}
-
-export const fetchAirQualityForCity = async (city: string) => {
-  const coords = cityCoordinates[city];
-  if (!coords) return null;
-
-  try {
-    const res = await instance.get(
-      `/air-quality?lat=${coords.lat}&lon=${coords.lon}`,
-    );
-    const responseText = res.data;
-
-    const json =
-      typeof responseText === "string"
-        ? JSON.parse(responseText)
-        : responseText;
-    return { city, data: json.data };
-  } catch (err) {
-    console.error(`Błąd dla ${city}:`, err);
-    return null;
-  }
-};
+import { AirQualityResponse, getAirQuality } from "@/api/airQualityApi";
 
 export const useAirQuality = (city: string) => {
-  const [data, setData] = useState<AirQualityData | null>(null);
+  const [data, setData] = useState<AirQualityResponse | null>(null);
   const [loading, setLoading] = useState(true);
-  const coords = cityCoordinates[city];
 
   useEffect(() => {
-    if (!coords) return;
-
     const fetchData = async () => {
       setLoading(true);
-      try {
-        const res = await instance.get(
-          `/air-quality?lat=${coords.lat}&lon=${coords.lon}`,
-        );
-        const responseText = res.data;
-        console.log(`API Response dla ${city}:`, responseText);
-        if (
-          typeof responseText === "string" &&
-          responseText.includes("namespace")
-        ) {
-          throw new Error("Wrong answer from backend");
-        }
 
-        const json =
-          typeof responseText === "string"
-            ? JSON.parse(responseText)
-            : responseText;
+      setData(await getAirQuality(city));
 
-        if (!json?.data) throw new Error("no 'data'");
-        setData(json.data);
-      } catch (err: any) {
-        console.error("API error:", err.message || err);
-        setData(null);
-      } finally {
-        setLoading(false);
-      }
+      console.log(`API Response dla ${city}:`, data);
+
+      setLoading(false);
     };
 
     fetchData();
-  }, [city]);
+  }, [city, data]);
 
   return { data, loading };
 };
