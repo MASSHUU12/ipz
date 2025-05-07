c418102b4d2ce0699c94e6f56b762d731fb2bc76
Maciej Gawrysiak <gawrysiak.maciej@proton.me>
2025-05-07 11:22:11 +0200

    fix(frontend): /synop response is not correctly handled

--- resources/js/api/synopApi.ts
+++ resources/js/api/synopApi.ts
@@ -1,34 +1,17 @@
 import { instance } from "./api";
 
 export interface SynopResponse {
-  id_stacji: string;
-  stacja: string;
-  data_pomiaru: string;
-  godzina_pomiaru: string;
-  temperatura: string;
-  predkosc_wiatru: string;
-  kierunek_wiatru: string;
-  wilgotnosc_wzgledna: string;
-  suma_opadu: string;
-  cisnienie: string;
-}
-
-export type SynopDataConverted = Omit<
-  SynopResponse,
-  | "temperatura"
-  | "predkosc_wiatru"
-  | "kierunek_wiatru"
-  | "wilgotnosc_wzgledna"
-  | "suma_opadu"
-  | "cisnienie"
-> & {
+  station_id: string;
+  station_name: string;
+  measurement_date: string;
+  measurement_hour: string;
   temperature: number;
   wind_speed: number;
   wind_direction: number;
   relative_humidity: number;
   rainfall_total: number;
   pressure: number;
-};
+}
 
 export const getSynop = async (
   station: string,
